14019d8f2494004a90afabf503d7d1ada553db90
Sander072 <sander072@mail.ru>
2024-05-18 14:32:23 +0300

    Добавьте файлы проекта.

---
+++ Lesson4.cpp
@@ -0,0 +1,14 @@
+﻿// Lesson4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::string a = "The fourth lesson";
+    std::cout << a << "\n";
+    std::cout << size(a) << "\n";
+    std::cout << a.substr(0, 1) << "\n";
+    std::cout << a.substr(16);
+}
+
